9d4c01d54649ce764168d26796980cb56fecbcc9
suzuka-horikoshi <suzuka.horikoshi@as-child.com>
2024-07-02 17:48:37 +0900

    Userモデルにバリデーションを追加

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX },
+                    presence: true,
+                    length: { maximum: 255 },
+                    uniqueness: true
+end
