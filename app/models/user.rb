74a8a2458f2f43738ffba5548c044b69458c4748
suzuka-horikoshi <suzuka.horikoshi@as-child.com>
2024-07-03 09:27:01 +0900

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
                     presence: true,
                     length: { maximum: 255 },
                     uniqueness: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
