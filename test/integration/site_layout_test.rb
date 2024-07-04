fffc43a11f22934794470fc509fe6520d8350bac
suzuka-horikoshi <suzuka.horikoshi@as-child.com>
2024-07-04 21:42:31 +0900

    User一覧ページを作成

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -8,8 +12,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    get contact_path
-    assert_select "title", full_title("Contact")
+    log_in_as(@user)
+    get edit_user_path(@user)
+    assert_select "a[href=?]", users_path
   end
 
   test "signup page" do
