f962128b7bf875e355ed8819d22b445019407eb9
suzuka-horikoshi <suzuka.horikoshi@as-child.com>
2024-07-04 11:58:13 +0900

    ユーザを忘れる機能を実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,7 +14,7 @@ module SessionsHelper
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.encrypted[:user_id])
       user = User.find_by(id: user_id)
-      if user && authenticeted?(cookies[:remember_token])
+      if user && user.authenticeted?(cookies[:remember_token])
         log_in user
         @current_user = user
       end
@@ -25,7 +25,14 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
   def log_out
+    forget(current_user)
     reset_session
     @current_user = nil # 安全のため
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -31,4 +31,8 @@ class User < ApplicationRecord
   def authenticeted?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
