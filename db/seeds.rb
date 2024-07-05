676e6228ea0c5b6d6ccffa305a47bf7d17c2a917
suzuka-horikoshi <suzuka.horikoshi@as-child.com>
2024-07-05 11:35:33 +0900

    authenticatd?メソッドを汎用化

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # メインのサンプルユーザーを1人作成する
-User.create!(name:  "Example User",
+User.create!(name:  "Example! User",
              email: "example@railstutorial.org",
              password:              "foobar",
              password_confirmation: "foobar",
