bdfc502a9d8bcd80742e52ad42bce27c8e8818a4
suzuka-horikoshi <suzuka.horikoshi@as-child.com>
2024-07-08 10:43:24 +0900

    logged_in_userフィルターを共通化

--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :password_resets, only: [:new, :create, :edit, :update]
+  resources :microposts, only: [:create, :destroy]
 end
